483055e2697520ef1e78a8df69906c6b62d27d35
Lambor <757016515@qq.com>
2017-12-19 13:57:36 +0800

    Update Onboard_RGB_LED.js

--- Onboard_RGB_LED/Onboard_RGB_LED.js
+++ Onboard_RGB_LED/Onboard_RGB_LED.js
@@ -1,2 +1,22 @@
+/*
 WioLTE.setLEDPower(true);
-WioLTE.LED(r,g,b); // please modify the RGB to values with range 0..255)
+WioLTE.LED(r,g,b); // r,g,b range between 0 to 255
+*/
+
+WioLTE.setLEDPower(true);
+
+var rgb = new Uint8ClampedArray(3);
+var pos = 0;
+function getPattern() {
+  pos++;
+  for (var i=0;i<rgb.length;) {
+    rgb[i++] = (1 + Math.sin((i+pos)*0.1324)) * 10;
+    rgb[i++] = (1 + Math.sin((i+pos)*0.1654)) * 10;
+    rgb[i++] = (1 + Math.sin((i+pos)*0.1)) * 10;
+  }
+  return rgb;
+}
+setInterval(function() {
+  var color = getPattern();
+  WioLTE.LED(color[0], color[1], color[2]);
+}, 100);
