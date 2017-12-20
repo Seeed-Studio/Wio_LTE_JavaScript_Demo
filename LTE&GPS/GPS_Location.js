a63562aa791e88e00c71159011bac3ee0503f592
Lambor <757016515@qq.com>
2017-12-20 16:48:13 +0800

    Update GPS_Location.js

--- LTE&GPS/GPS_Location.js
+++ LTE&GPS/GPS_Location.js
@@ -1,40 +1,20 @@
-digitalWrite(B2, 1); // Set the modem  power pin when Wio LTE board under v1.2
+digitalWrite(B2, 1);
 var board;
-var APN = "CMNET";
-var USERNAME = "";
-var PASSWORD = "";
 
 function wiolteStart(debug_quectel, debug_at) {
-  debug_quectel = debug_quectel || false;
-  debug_at = debug_at || false;
-
   board = require('wiolte').connect(function(err) {
     console.log("connectCB entered...");
-    if (err) throw err;
-    setTimeout(doConnect,3000);
+    if (err) console.log(err);
+    setTimeout(onStart,3000);
   });
-
   board.debug(debug_quectel, debug_at);
-
 }
 
-function doConnect() {
-  board.connect(APN, USERNAME, PASSWORD, function(err) {
-    console.log("connectCB entered...");
-    if (err) throw err;
-    board.getIP(print);
-
-    // work after connected
-    setTimeout(onConnected, 5000);
-
-  });
-}
-
-function onConnected(){
+function onStart(){
   // fetch longitude, latitude every 10 s
   board.geoLocStart(10000);
+  setInterval(GeoLoc, 2000);
 }
-
 
 function GeoLoc() {
   var coord="";
